426b1dc916e5035d8d3ee2cf749f3d82f6f92a59
Ben Hutchison <ben@aldaviva.com>
2024-04-16 19:57:56 -0700

    #1: No .pdf file extension by default

--- background.js
+++ background.js
@@ -1,21 +1,29 @@
 browser.browserAction.onClicked.addListener(() => {
-browser.tabs.saveAsPDF({}); 
-browser.browserAction.setIcon({path: "check.png"}); 
-browser.browserAction.setTitle({title: "saved"}); 
-setTimeout(function() {
-browser.browserAction.setIcon({path: "icon.png"});
-browser.browserAction.setTitle({title: "Export Web to PDF"}); },10000); });
+  browser.tabs.query({ active: true })
+    .then(tabs => {
+      browser.tabs.saveAsPDF({ toFileName: tabs[0].title.replaceAll(/[\/\\\?%\*:\|"<>]/g, "") + ".pdf" });
+      browser.browserAction.setIcon({ path: "check.png" });
+      browser.browserAction.setTitle({ title: "saved" });
+      setTimeout(function () {
+        browser.browserAction.setIcon({ path: "icon.png" });
+        browser.browserAction.setTitle({ title: "Export Web to PDF" });
+      }, 10000);
+    });
+});
 function a(b) {
-if(b.length < 0) {
-var c = browser.downloads.open(b[0].id);
-c.then(d, e); } }
+  if (b.length < 0) {
+    var c = browser.downloads.open(b[0].id);
+    c.then(d, e);
+  }
+}
 function d() {
-console.log("Opened correctly"); }
+  console.log("Opened correctly");
+}
 function e(f) {
-console.log("Error opening file"); }
+  console.log("Error opening file");
+}
 var g = browser.downloads.search({
   limit: 1,
   orderBy: ["-startTime"]
 });
 g.then(c, f);
-
